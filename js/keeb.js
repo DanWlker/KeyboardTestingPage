1d2769d5b0d0e43cb47de2b1f88ddf3d0f728c7e
DanWlker <danielhee2@gmail.com>
2022-01-31 01:14:58 +0800

    Fixed bug for PrintScreen not showing up

--- js/keeb.js
+++ js/keeb.js
@@ -62,9 +62,16 @@
             //     }
             // }
 
-            if(pressedKey == 'PrintScreen')
+            if(pressedKey == 'PrintScreen') {
                 if(!document.getElementById(pressedKey).classList.contains("pressed")) 
-                    document.getElementById(pressed).className += " pressed ";
+                    document.getElementById(pressedKey).className += " pressed ";
+                
+                let tempDiv = document.createElement("div");
+                tempDiv.className = "logBox";
+                tempDiv.textContent = e.key;
+                document.getElementById("logHolder").prepend(tempDiv);
+            }
+                
             
             if(pressedKey === "MetaLeft")
                 pressedKey = "OSLeft";
